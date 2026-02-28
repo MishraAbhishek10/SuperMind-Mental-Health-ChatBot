fdd6416b7a95c5b76fa0a7d9261f649fefa95419
Abhishek Mishra <amishra.aicc@gmail.com>
2026-02-28 21:15:07 +0530

    Modify TTS initialization based on environment
    Refactor voice_output.py to conditionally initialize TTS engine based on IS_CLOUD variable.

--- voice_output.py
+++ voice_output.py
@@ -1,18 +1,33 @@
 
-#import pyttsx3
-import threading
+# import pyttsx3
+# import threading
 
-engine = pyttsx3.init()
-engine.setProperty("rate", 160)
+# engine = pyttsx3.init()
+# engine.setProperty("rate", 160)
 
-def speak(text):
-    pass
-    def run_tts():
+# def speak(text):
+#     pass
+#     def run_tts():
+#         engine.say(text)
+#         engine.runAndWait()
+
+#     t = threading.Thread(target=run_tts)
+#     t.start()
+import os
+
+IS_CLOUD = True   # set False locally
+
+if IS_CLOUD:
+    def speak(text):
+        pass
+else:
+    import pyttsx3
+    engine = pyttsx3.init()
+    engine.setProperty("rate", 160)
+
+    def speak(text):
         engine.say(text)
         engine.runAndWait()
-
-    t = threading.Thread(target=run_tts)
-    t.start()
 
 
 
