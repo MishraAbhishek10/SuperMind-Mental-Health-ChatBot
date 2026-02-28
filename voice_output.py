23a8b4f42b6196779b63a699623013a278f7f0f5
Abhishek Mishra <amishra.aicc@gmail.com>
2026-02-28 21:13:00 +0530

    Update voice_output.py

--- voice_output.py
+++ voice_output.py
@@ -6,12 +6,13 @@ engine = pyttsx3.init()
 engine.setProperty("rate", 160)
 
 def speak(text):
-    #pass
+    pass
     def run_tts():
         engine.say(text)
         engine.runAndWait()
 
     t = threading.Thread(target=run_tts)
     t.start()
+
 
 
